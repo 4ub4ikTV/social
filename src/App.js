4bbbc1d7788fb13efd5f10a13df128f451788263
4ub4ik <svyantkoorest@gmail.com>
2023-06-06 18:42:09 +0300

    update project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import './App.css';
-import {Header} from "./Header";
-import {Technologies} from "./Technologies";
 
 function App() {
-  return (
-    <div>
-      <Header/>
-      <Technologies/>
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <header className="header">
+                <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt="logo"/>
+            </header>
+            <nav className="nav">
+                <div><a href="">Profile</a></div>
+                <div><a href="">Messages</a></div>
+                <div><a href="">News</a></div>
+                <div><a href="">Music</a></div>
+                <div><a href="">Settings</a></div>
+            </nav>
+            <div className="content">
+                <img
+                    src="https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1295,h_720/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/t9ur9cc1khkup1dmcbzd/IMGWorldsofAdventureTicketinDubai.webp"
+                    alt="content"/>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
