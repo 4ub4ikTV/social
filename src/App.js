87b81826cfbd6fa05451830d7e4bfe86d1561e1f
4ub4ik <svyantkoorest@gmail.com>
2023-06-14 19:42:46 +0300

    update project

--- src/App.js
+++ src/App.js
@@ -6,26 +6,8 @@ import {Navbar} from "./Navbar/Navbar";
 import {Profile} from "./Profile/Profile";
 import {Dialogs, Music, Settings} from "./components";
 
-const dialog = [
-    {id: 1, name: "Orest"},
-    {id: 2, name: "Nadia"},
-    {id: 3, name: "Vlad"},
-    {id: 4, name: "Nazar"},
-    {id: 5, name: "Vira"},
-]
 
-const message = [
-    {message: "Hi"},
-    {message: "How is your it-kamasutra?"},
-    {message: "Bye!"}
-]
-
-const post = [
-    {message: "Hi how are you", like: "0"},
-    {message: "Its my first project", like: "23"}
-]
-
-function App() {
+function App(props) {
     return (
         <BrowserRouter>
             <div className="app-wrapper">
@@ -33,8 +15,8 @@ function App() {
                 <Navbar/>
                 <div className="app-wrapper-item">
                     <Routes>
-                        <Route path="/profile" element={<Profile post={post}/>}/>
-                        <Route path="/messages/*" element={<Dialogs dialog={dialog} message={message}/>}/>
+                        <Route path="/profile" element={<Profile post={props.post}/>}/>
+                        <Route path="/messages/*" element={<Dialogs dialog={props.dialog} message={props.message}/>}/>
                         <Route path="/music" element={<Music/>}/>
                         <Route path="/settings" element={<Settings/>}/>
                     </Routes>
